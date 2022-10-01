2297e98334a87382e6c4ad56300b863d22fab329
thinhtethtetzaw778 <thinhtethtetzaw778@gmail.com>
2022-10-01 18:31:12 +0630

    Setup tailwind and icon

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { FaBeer } from 'react-icons/fa';
+
 export default function Home() {
   return (
-    <div>
+    <div className="text-red-500">
       Hello
+      <FaBeer className='w-10 h-10'/>
     </div>
   )
 }
